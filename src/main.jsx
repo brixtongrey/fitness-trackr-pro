3583773628e6c061bbed9714b1fb70ee6fb256bd
Brixtongrey <brixtonaruda@gmail.com>
2025-09-28 09:03:29 -0600

    feat: login + register updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,14 @@ import App from "./App.jsx";
 import Layout from "./layout/Layout.jsx";
 
 import { AuthProvider } from "./auth/AuthContext";
-// import { PageProvider } from "./layout/PageContext";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
+    <BrowserRouter>
       <Layout>
-        <BrowserRouter>
         <App />
-        </BrowserRouter>
       </Layout>
-  </AuthProvider>,
+    </BrowserRouter>
+  </AuthProvider>
 );
